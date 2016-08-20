8cb970cbf4269e9a458c17c6f08c56be9b51f9ed
Udgin Pyl <gromkaktus@gmail.com>
2016-08-20 23:28:38 +0200

    Added one test

---
+++ TorrentUWP.Tests/BEncodedListTets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TorrentUWP.BEncoding;
+using Xunit;
+
+namespace TorrentUWP.Tests
+{
+    public class BEncodedListTets
+    {
+        private readonly BEncodedList _target;
+
+        public BEncodedListTets()
+        {
+            _target = new BEncodedList();
+        }
+
+        [Fact]
+        public void ShouldAddValueToCollection()
+        {
+            var item = new BEncodedString(string.Empty);
+            _target.Add(item);
+
+            Assert.Contains(_target, x => x == item);
+        }
+    }
+}
